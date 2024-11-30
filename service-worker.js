fb6fcf16b835c0755dc716107fa02cdc025b8b94
theb0i <82817054+theb0i@users.noreply.github.com>
2024-11-30 20:20:53 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-// Einfacher Service Worker
-self.addEventListener('install', event => {
-    console.log('Service Worker installiert.');
-});
-
-self.addEventListener('activate', event => {
-    console.log('Service Worker aktiviert.');
-});
